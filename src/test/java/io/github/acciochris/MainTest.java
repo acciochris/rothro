f8aeed0fd3d67806ecc4e3bb8789c5258bcae2c8
Chris Liu <acciochris@outlook.com>
2025-04-27 21:04:44 -0700

    Add initial tests

---
+++ src/test/java/io/github/acciochris/MainTest.java
@@ -0,0 +1,15 @@
+package io.github.acciochris;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+class MainTests {
+
+    private final Main main = new Main();
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+}
