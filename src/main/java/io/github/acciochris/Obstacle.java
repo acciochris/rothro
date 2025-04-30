ad08e40de282c1e27ae4d472b652ec1fc7e14495
AnveshKrishna <anveshcgess@gmail.com>
2025-04-30 12:45:38 -0700

    Implement Obstacle

--- src/main/java/io/github/acciochris/Obstacle.java
+++ src/main/java/io/github/acciochris/Obstacle.java
@@ -1,10 +1,29 @@
 package io.github.acciochris;
 
-import org.dyn4j.geometry.Convex;
+import org.dyn4j.geometry.*;
 import io.github.acciochris.framework.SimulationBody;
 
-public class Obstacle extends SimulationBody {
+
+public class Obstacle extends SimulationBody 
+{
+    private double x;
+    private double y;
+
     public Obstacle(Convex shape, double x, double y) {
-        
+        addFixture(shape);
+        translate(x, y);
+        this.x = x;
+        this.y = y;
+        setMass(MassType.INFINITE);
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
     }
 }
