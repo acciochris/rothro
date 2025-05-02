b4be38f50dd60c0393251a8064e2abea679d7c73
JustinHuang <justfhuang@gmail.com>
2025-05-02 12:31:24 -0700

    Create constructor and basic fields for Ball class

--- src/main/java/io/github/acciochris/Ball.java
+++ src/main/java/io/github/acciochris/Ball.java
@@ -1,7 +1,47 @@
 package io.github.acciochris;
 
+import java.util.ArrayList;
+import java.awt.event.KeyEvent;
+import org.dyn4j.collision.Filter;
+import org.dyn4j.dynamics.BodyFixture;
+import org.dyn4j.geometry.Geometry;
+import org.dyn4j.geometry.MassType;
+import org.dyn4j.geometry.Polygon;
+import org.dyn4j.geometry.Circle;
+import org.dyn4j.geometry.Transform;
+import org.dyn4j.geometry.Vector2;
+import io.github.acciochris.framework.Camera;
 import io.github.acciochris.framework.SimulationBody;
+import io.github.acciochris.framework.SimulationFrame;
+import io.github.acciochris.framework.input.BooleanStateKeyboardInputHandler;
+import io.github.acciochris.framework.input.Key;
+import org.dyn4j.world.World;
 
-public class Ball extends SimulationBody {
-    public Ball() {}
+public class Ball extends SimulationBody 
+{
+    private SimulationBody ball;
+    private ArrayList<ArrayList<String>> controls;
+
+    private BooleanStateKeyboardInputHandler left;
+	private BooleanStateKeyboardInputHandler right;
+	
+	private BooleanStateKeyboardInputHandler plus;
+	private BooleanStateKeyboardInputHandler minus;
+	
+	private BooleanStateKeyboardInputHandler shoot;
+
+    public Ball() 
+    {
+        ball = new SimulationBody();
+        ball.addFixture(new Circle(1));
+        ball.setMass(MassType.NORMAL);
+        controls = new ArrayList<ArrayList<String>>(5);
+    }
+    public void setControls()
+    {
+        
+    }
+    public void control()
+    {
+    }
 }
