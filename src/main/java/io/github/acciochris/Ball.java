51ebf136b849de5ce8ca77ff7ee2d89ee2d9ce39
JustinHuang <justfhuang@gmail.com>
2025-05-10 00:23:26 -0700

    Created basic controls

--- src/main/java/io/github/acciochris/Ball.java
+++ src/main/java/io/github/acciochris/Ball.java
@@ -1,7 +1,9 @@
 package io.github.acciochris;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import org.dyn4j.collision.Filter;
 import org.dyn4j.dynamics.BodyFixture;
 import org.dyn4j.geometry.Geometry;
@@ -17,9 +19,9 @@ import io.github.acciochris.framework.input.BooleanStateKeyboardInputHandler;
 import io.github.acciochris.framework.input.Key;
 import org.dyn4j.world.World;
 
-public class Ball extends SimulationBody 
+public class Ball extends SimulationBody
 {
-    private ArrayList<ArrayList<String>> controls;
+    private ArrayList<ArrayList<Integer>> controls;
 
     private BooleanStateKeyboardInputHandler left;
 	private BooleanStateKeyboardInputHandler right;
@@ -34,13 +36,36 @@ public class Ball extends SimulationBody
         addFixture(new Circle(1));
         setMass(MassType.NORMAL);
         setAngularDamping(1000.0);
-        controls = new ArrayList<ArrayList<String>>(5);
+        controls = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= 5; i++)
+        {
+            controls.add(new ArrayList<Integer>());
+        }
+        controls.get(0).add(KeyEvent.VK_LEFT);
+        controls.get(1).add(KeyEvent.VK_RIGHT);
+        controls.get(2).add(KeyEvent.VK_SPACE);
     }
     public void setControls(String control)
     {
         
     }
-    public void control()
+    public void controls(HashSet<Integer> keysPressed)
     {
+        int angle = 90;
+        for (int i:keysPressed)
+        {
+            while(controls.get(0).contains(i))
+            {
+                angle += 0.005;
+            }
+            while(controls.get(1).contains(i))
+            {
+                angle -= 0.005;
+            }
+            if(controls.get(2).contains(i))
+            {
+                setLinearVelocity(new Vector2(Math.cos(angle), Math.sin(angle)));
+            }
+        }
     }
 }
