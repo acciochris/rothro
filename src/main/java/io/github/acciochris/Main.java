6b3021b972d6d50937d8a5e582670d4da04527b9
AnveshKrishna <anveshcgess@gmail.com>
2025-05-16 11:27:00 -0700

    Add level1 & 2

--- src/main/java/io/github/acciochris/Main.java
+++ src/main/java/io/github/acciochris/Main.java
@@ -11,8 +11,23 @@ public class Main {
     {
         Level level = new Level();
         level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, 3.0));
-        level.addObstacle(new Obstacle(new Circle(3.0), 7.0, 6.0));
+        level.addObstacle(new Obstacle(new Rectangle(1.0, 2.0), 5, -8.0));
+        level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, -5.0));
+        level.addObstacle(new Obstacle(new Circle(2.0), 5.0, 10.0));
+        level.addObstacle(new Obstacle(new Capsule(5.0, 2.0), 9.0, 6.0));
+        level.addObstacle(new Obstacle(new Segment(new Vector2(-1.0, 4.0), new Vector2(5.0, 8.0)), 7.0, -9.0));
+        level.setHole(new Hole(8.0, 3.0));
         return level;
+    }
+
+    public static Level level1()
+    {
+        return null; // FIX THIS!
+    }
+
+    public static Level level2()
+    {
+        return null; // FIX THIS!
     }
 
     public static void main(String[] args) {
