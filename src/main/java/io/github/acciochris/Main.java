ebd705e3fed02bf0785081f0aa17f44db1c28045
AnveshKrishna <anveshcgess@gmail.com>
2025-05-16 12:41:44 -0700

    Set Ball position

--- src/main/java/io/github/acciochris/Main.java
+++ src/main/java/io/github/acciochris/Main.java
@@ -11,14 +11,14 @@ public class Main {
     {
         Level level = new Level();
         level.setBallRadius(1.5);
+        level.setBallPos(new Vector2(-10, -5));
+        level.setHole(new Hole(8.0, level.getBallRadius() * 2 + 1.0));
         level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, 3.0));
         level.addObstacle(new Obstacle(new Rectangle(1.0, 2.0), 5, -8.0));
         level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, -5.0));
         level.addObstacle(new Obstacle(new Circle(2.0), 5.0, 10.0));
         level.addObstacle(new Obstacle(new Capsule(5.0, 2.0), 9.0, 6.0));
         level.addObstacle(new Obstacle(new Segment(new Vector2(-1.0, 4.0), new Vector2(5.0, 8.0)), 7.0, -9.0));
-        level.setBallPos(new Vector2(-10, -5));
-        level.setHole(new Hole(8.0, level.getBallRadius() * 2 + 1.0));
         return level;
     }
 
@@ -36,12 +36,7 @@ public class Main {
     {
         Level level = new Level();
         level.setBallRadius(0.5);
-        level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, 3.0));
-        level.addObstacle(new Obstacle(new Rectangle(1.0, 2.0), 5, -8.0));
-        level.addObstacle(new Obstacle(new Rectangle(2.0, 1.0), 3.0, -5.0));
-        level.addObstacle(new Obstacle(new Circle(2.0), 5.0, 10.0));
-        level.addObstacle(new Obstacle(new Capsule(5.0, 2.0), 9.0, 6.0));
-        level.addObstacle(new Obstacle(new Segment(new Vector2(-1.0, 4.0), new Vector2(5.0, 8.0)), 7.0, -9.0));
+        level.setBallPos(new Vector2(-3, -9));
         level.setHole(new Hole(8.0, level.getBallRadius() * 2 + 0.2));
         return level;
     }
