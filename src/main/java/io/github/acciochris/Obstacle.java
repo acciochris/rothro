731397e8dc8dff53912de6b04100d249f8769e9f
Chris Liu <acciochris@outlook.com>
2025-05-23 12:17:14 -0700

    Add javadocs to Obstacle

--- src/main/java/io/github/acciochris/Obstacle.java
+++ src/main/java/io/github/acciochris/Obstacle.java
@@ -6,7 +6,13 @@ import java.awt.Color;
 import java.awt.Point;
 import io.github.acciochris.framework.SimulationBody;
 
-
+/**
+ * Represents an obstacle in a level. Extends SimulationBody.
+ * 
+ * @author Chris Liu
+ * @author Anvesh Pattaje
+ * @version May 30, 2025
+ */
 public class Obstacle extends SimulationBody 
 {
     private double x;
@@ -15,8 +21,23 @@ public class Obstacle extends SimulationBody
     private String jointType;
     private Convex shape;
     private String massType;
+
+    /**
+     * Default color for obstacles.
+     */
     public static final Color DEFAULT_COLOR = new Color(0x6272a4);
 
+    /**
+     * Construct a new Obstacle from various parameters.
+     * 
+     * @param shape a Convex shape for the obstacle
+     * @param x x coordinate of the obstacle
+     * @param y y coordinate of the obstacle
+     * @param color color of the obstacle
+     * @param canMove boolean parameter for whether the obstacle can move
+     * @param jT joint type for this obstacle
+     * @param mT mass type for this obstacle
+     */
     public Obstacle(Convex shape, double x, double y, Color color, boolean canMove, String jT, String mT) {
         super(color);
         this.shape = shape;
@@ -55,30 +76,63 @@ public class Obstacle extends SimulationBody
         setAtRestDetectionEnabled(false);
     }
 
+    /**
+     * Simplified constructor for obstacles without colors or joints.
+     * 
+     * @param shape a Convex shape for the obstacle
+     * @param x x coordinate of the obstacle
+     * @param y y coordinate of the obstacle
+     * @param canMove boolean parameter for whether the obstacle can move
+     */
     public Obstacle(Convex shape, double x, double y, boolean canMove) {
         this(shape, x, y, DEFAULT_COLOR, canMove, "", "");
     }
 
+    /**
+     * Getter for x coordinate.
+     * 
+     * @return x coordinate
+     */
     public double getX()
     {
         return x;
     }
 
+    /**
+     * Getter for y coordinate.
+     * 
+     * @return y coordinate
+     */
     public double getY()
     {
         return y;
     }
 
+    /**
+     * Getter for joint type.
+     * 
+     * @return joint type
+     */
     public String getJointType()
     {
         return jointType;
     }
 
+    /**
+     * Getter for shape.
+     * 
+     * @return Convex shape
+     */
     public Convex getShape()
     {
         return shape;
     }
 
+    /**
+     * Getter for mass type.
+     * 
+     * @return mass type
+     */
     public String getMassType()
     {
         return massType;
