a7185d6d435559e8f613373248fcd01493eda987
Chris Liu <acciochris@outlook.com>
2025-05-26 20:41:31 -0700

    Add javadocs to RothroKeyListener

--- src/main/java/io/github/acciochris/RothroKeyListener.java
+++ src/main/java/io/github/acciochris/RothroKeyListener.java
@@ -3,31 +3,61 @@ package io.github.acciochris;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+/**
+ * Key listener for the RoThro game. Implements java.awt.event.KeyListener.
+ * 
+ * @author Justin Huang
+ * @version May 30, 2025
+ */
 public class RothroKeyListener implements KeyListener
 {
+    private RothroInputManager im;
 
-    RothroInputManager im;
+    /**
+     * Construct a new RothroKeyListener.
+     */
     public RothroKeyListener()
     {
         im = new RothroInputManager();
     }
+    
+    /**
+     * Forward key presses to input manager.
+     * 
+     * @param e the key event
+     */
     @Override
     public void keyPressed(KeyEvent e)
     {
         im.keyPressed(e.getKeyCode());
         
     }
+
+    /**
+     * Forward key releases to input manager.
+     * 
+     * @param e the key event
+     */
     @Override
     public void keyReleased(KeyEvent e)
     {
         im.keyReleased(e.getKeyCode());
     }
-    @Override
-    /*
-     * keyTyped is uneeded for this project
-     */
-    public void keyTyped(KeyEvent e){}
 
+
+    /**
+     * keyTyped is unused for this project.
+     * 
+     * @param e unused
+     */
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    /**
+     * Getter for input manager
+     * 
+     * @return the input manager
+     */
     public RothroInputManager getIm()
     {
         return im;
