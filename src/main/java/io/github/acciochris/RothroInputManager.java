c88c1a1fc431e9bb637c67b477671dd38bd55215
Chris Liu <acciochris@outlook.com>
2025-05-26 20:47:10 -0700

    Add javadocs for RothroInputManager

--- src/main/java/io/github/acciochris/RothroInputManager.java
+++ src/main/java/io/github/acciochris/RothroInputManager.java
@@ -2,25 +2,49 @@ package io.github.acciochris;
 
 import java.util.HashSet;
 
-//Fuckin glorified list, I can't believe KeyListener won't allow me to just return KeyEvent values
+/**
+ * Key input manager for Rothro.
+ * 
+ * @author Justin Huang
+ * @version May 30, 2025
+ */
 public class RothroInputManager 
 {
     private HashSet<Integer> keysPressed;
+
+    /**
+     * Construct a new RothroInputManager with no keys depressed.
+     */
     public RothroInputManager()
     {
         keysPressed = new HashSet<Integer>();
     }
 
+    /**
+     * Add new pressed key.
+     * 
+     * @param i keyCode of pressed key
+     */
     public void keyPressed(int i)
     {
         keysPressed.add(i);
     }
 
+    /**
+     * Remove existing pressed key.
+     * 
+     * @param i keyCode of pressed key
+     */
     public void keyReleased(int i)
     {
         keysPressed.remove(i);
     }
 
+    /**
+     * Getter for keysPressed
+     * 
+     * @return HashSet of keys that are currently depressed
+     */
     public HashSet<Integer> getKeysPressed()
     {
         return keysPressed;
